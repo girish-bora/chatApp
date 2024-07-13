03fb576ca87c01204581f94e10c845ea59a538e4
Girish <girishh.bora@gmail.com>
2024-07-13 10:02:35 +0530

    added login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import Chat from "./components/Chat/Chat";
 import Detail from "./components/Detail/Detail";
 import List from "./components/List/List";
+import Login from "./components/Login/Login";
 
 const App = () => {
+  const user = false;
+
   return (
     <div className="container">
-      <List></List>
-      <Chat></Chat>
-      <Detail></Detail>
+      {user ? (
+        <>
+          <List></List>
+          <Chat></Chat>
+          <Detail></Detail>
+        </>
+      ) : (
+        <Login></Login>
+      )}
     </div>
   );
 };
